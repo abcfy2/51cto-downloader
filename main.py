1bfa375a173aa0e94ef300ca5c2ddf0c0e3c4ef7
Feng Yu <fyu@ok988.com>
2014-06-11 09:32:20 +0800

    删除无用的打印信息

--- main.py
+++ main.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python
 #coding:utf-8
+
+'''源码仅用于学习交流，请勿非法使用！'''
+
 import requests
 import getpass
 import re
 
 def login(user, password):
-    #cj = cookielib.CookieJar()
+    '''模拟登录请求，成功返回cookiesjar类型，否则返回空'''
     loginPostURL = 'http://home.51cto.com/index.php?s=/Index/doLogin'
     loginHeaders = {'User-Agent': ' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'}
     postData = {'email' : user,
                 'passwd' : password,
                 'authlogin' : 'on'}
     loginSession = requests.Session()
-    #loginSession.cookies = cj
     loginSession.headers = loginHeaders
     loginRequest = loginSession.post(loginPostURL, data = postData)
-    print 'Response Headers:'
-    print loginRequest.headers
-    print ''
-    print 'Request Headers:'
-    print loginRequest.request.headers
-    print ''
-    print 'Body:'
-    print loginRequest.text
-    print ''
-    print 'Cookies:'
-    print loginSession.cookies
     if re.match(r'^<script', loginRequest.text):
         print '登录成功！'
+        print '获取cookies中……'
         pattern = re.compile(r'src="(http://\w+?\.51cto\.com.+?)"')
         for apiURL in pattern.findall(loginRequest.text):
             apiURLRequest = loginSession.get(apiURL)
-            print apiURLRequest.text
-            print apiURLRequest.headers
-            print apiURLRequest.request.headers
-            print apiURLRequest.cookies
+        print '获取cookies完毕'
         return loginSession.cookies
     else:
         print '登录失败！'
     loginSession.close()
 
 def requestEdu(cj):
+    '''模拟edu.51cto.com登录'''
     eduURL = 'http://edu.51cto.com/'
     requestHeaders = {'User-Agent': ' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'}
     requestSession = requests.Session()
@@ -55,7 +45,8 @@ def main():
     user = raw_input('user: ')
     password = getpass.getpass('password: ')
     cj = login(user, password)
-    requestEdu(cj)
+    if cj:
+        requestEdu(cj)
 
 if __name__ == '__main__':
     main()
