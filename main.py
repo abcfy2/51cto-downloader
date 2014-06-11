2250c8a87bce953315f00bf72030c6de2835e8c7
Feng Yu <fyu@ok988.com>
2014-06-11 09:09:32 +0800

    windows终端显示乱码，将乱码部分改为英文

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ def requestEdu(cj):
 
 
 def main():
-    user = raw_input('请输入用户名: ')
-    password = getpass.getpass('请输入密码: ')
+    user = raw_input('user: ')
+    password = getpass.getpass('password: ')
     cj = login(user, password)
     requestEdu(cj)
 
